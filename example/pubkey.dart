c615bf7b0ca43a15d1061d618d0c422ac968e407
xuty <xty50337@hotmail.com>
2022-01-08 00:01:42 +0800

    pubkey example

---
+++ example/pubkey.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dartssh2/dartssh2.dart';
+
+void main(List<String> args) async {
+  final socket = await SSHSocket.connect('localhost', 22);
+
+  final client = SSHClient(
+    socket,
+    username: 'root',
+    identities: [
+      // A single private key file may contain multiple keys.
+      ...SSHKeyPair.fromPem(await File('path/to/id_rsa').readAsString())
+    ],
+  );
+
+  final uptime = await client.run('uptime');
+  print(utf8.decode(uptime));
+
+  client.close();
+  await client.done;
+}
