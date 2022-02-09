4060f3dd1aade30dc81b0fa53e27b27e23d0697c
xuty <xty50337@hotmail.com>
2022-02-09 22:55:16 +0800

    Fix js import path #18

--- lib/src/socket/ssh_socket.dart
+++ lib/src/socket/ssh_socket.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:dartssh2/src/socket/ssh_socket_io.dart'
-    if (dart.library.js) 'package:dartssh2/next/socket/ssh_socket_js.dart';
+    if (dart.library.js) 'package:dartssh2/src/socket/ssh_socket_js.dart';
 
 abstract class SSHSocket {
   static Future<SSHSocket> connect(
