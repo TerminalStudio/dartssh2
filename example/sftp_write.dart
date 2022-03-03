f17f176f1c70b2d2b0768cecb864af69cb84a650
xuty <xty50337@hotmail.com>
2022-03-03 22:11:34 +0800

    Add sftp_write example

---
+++ example/sftp_write.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dartssh2/dartssh2.dart';
+
+void main(List<String> args) async {
+  final socket = await SSHSocket.connect('localhost', 22);
+
+  final client = SSHClient(
+    socket,
+    username: 'root',
+    onPasswordRequest: () {
+      stdout.write('Password: ');
+      stdin.echoMode = false;
+      return stdin.readLineSync() ?? exit(1);
+    },
+  );
+
+  final sftp = await client.sftp();
+
+  final file = await sftp.open(
+    '/root/test.txt',
+    mode: SftpFileOpenMode.create | SftpFileOpenMode.write,
+  );
+
+  await file.write(
+    Stream.value(Utf8Encoder().convert('hello there!')),
+  );
+
+  await file.close();
+
+  client.close();
+  await client.done;
+}
