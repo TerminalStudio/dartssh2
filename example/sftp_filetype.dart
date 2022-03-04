9fa43a22320a43d09fb22ced07cd2ddbe58958ef
xuty <xty50337@hotmail.com>
2022-03-04 16:41:23 +0800

    Add sftp_filetype example

---
+++ example/sftp_filetype.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:dartssh2/dartssh2.dart';
+
+void main(List<String> args) async {
+  final socket = await SSHSocket.connect('localhost', 22);
+
+  final client = SSHClient(
+    socket,
+    username: 'root',
+    onPasswordRequest: () {
+      stdout.write('Password: ');
+      stdin.echoMode = false;
+      return stdin.readLineSync() ?? exit(1);
+    },
+  );
+
+  final sftp = await client.sftp();
+  final files = await sftp.listdir('/');
+
+  for (var file in files) {
+    print('${file.filename} (${file.attr.type?.name})');
+  }
+
+  client.close();
+  await client.done;
+}
